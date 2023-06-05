e9bbfb3fc13780ef281c0f427f4ae5a74db38040
anshu57 <gangwaranshu3@gmail.com>
2023-06-05 09:29:06 +0530

    BST creation/insertion one

---
+++ Trees/BST.py
@@ -0,0 +1,50 @@
+
+
+
+class BSTNode:
+    def __init__(self, data):
+        self.data = data
+        self.leftChild = None
+        self.rightChild = None
+
+
+def insertNode(rootNode, nodeValue):
+    if rootNode.data == None:
+        rootNode.data = nodeValue
+    elif nodeValue <= rootNode.data:
+        if rootNode.leftChild is None:
+            rootNode.leftChild = BSTNode(nodeValue)
+        else:
+            insertNode(rootNode.leftChild, nodeValue)
+    else:
+        if rootNode.rightChild is None:
+            rootNode.rightChild =BSTNode(nodeValue)
+        else:
+            insertNode(rootNode.rightChild, nodeValue)
+    return "The Node has been successfully inserted"
+
+
+# newBST = BSTNode(None)
+# print(insertNode(newBST, 70))
+# print(insertNode(newBST,30))
+# print(newBST.data)
+# print(newBST.leftChild.data)
+
+def preOrderTraversal(rootNode):
+    if not rootNode:
+        return
+    print(rootNode.data)
+    preOrderTraversal(rootNode.leftChild)
+    preOrderTraversal(rootNode.rightChild)
+
+
+newBST = BSTNode(None)
+print(insertNode(newBST, 70))
+print(insertNode(newBST,30))
+insertNode(newBST,50)
+insertNode(newBST,20)
+insertNode(newBST,10)
+insertNode(newBST,180)
+insertNode(newBST,40)
+
+preOrderTraversal(newBST)
