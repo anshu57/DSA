d99109b082536f94cb6b533271be942c3da0b4fa
anshu57 <gangwaranshu3@gmail.com>
2023-07-06 09:53:14 +0530

    quick_sort completed

--- sorting/SortingAlgorithm.py
+++ sorting/SortingAlgorithm.py
@@ -118,6 +118,36 @@ def mergeSort(customList, l,r):
         merge(customList, l, m, r)
     return customList
 
-cList = [2,1,7,6,5,3,4,9,8]
+# cList = [2,1,7,6,5,3,4,9,8]
 
-print(mergeSort(cList, 0, 8))
+# print(mergeSort(cList, 0, 8))
+
+# Time Complexity : O(NlogN)
+# Space Complexity : O(N)
+
+
+def swap(my_list, index1, index2):
+    my_list[index1], my_list[index2] = my_list[index2],  my_list[index1]
+
+
+def pivot(my_list, pivot_index, end_index):
+    swap_index = pivot_index
+    for i in range(pivot_index+1, end_index+1):
+        if my_list[i] < my_list[pivot_index]:
+            swap_index +=1
+            swap(my_list, swap_index, i)
+    swap(my_list, pivot_index, swap_index)
+    return swap_index
+
+
+def quicksort(my_list, left, right):
+    if left<right:
+        pivot_index = pivot(my_list, left, right)
+        quicksort(my_list, left, pivot_index-1)
+        quicksort(my_list, pivot_index+1, right)
+
+
+my_list = [3,5,0,6,2,1,4]
+
+print(quicksort(my_list, 0, 6))
+print(my_list)
