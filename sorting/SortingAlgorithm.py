c8d8f51af02cf300821b39af49a52ca423357f95
anshu57 <gangwaranshu3@gmail.com>
2023-07-07 08:59:54 +0530

    Sorting completed

--- sorting/SortingAlgorithm.py
+++ sorting/SortingAlgorithm.py
@@ -147,7 +147,35 @@ def quicksort(my_list, left, right):
         quicksort(my_list, pivot_index+1, right)
 
 
-my_list = [3,5,0,6,2,1,4]
 
-print(quicksort(my_list, 0, 6))
+
+# print(quicksort(my_list, 0, 6))
+# print(my_list)
+
+def heapify(customList, n, i):
+    smallest = i
+    l = 2*i + 1
+    r = 2*i + 2
+    if l<n and customList[l] < customList[smallest]:
+        smallest = l
+    if r<n and customList[r] < customList[smallest]:
+        smallest = r
+    
+    if smallest != i:
+        customList[i], customList[smallest] = customList[smallest], customList[i]
+        heapify(customList, n, smallest)
+
+def heapSort(customList):
+    n = len(customList)
+    for i in range(int(n/2)-1, -1, -1):
+        heapify(customList, n, i)
+    for i in range(n-1, 0, -1):
+        customList[i], customList[0] = customList[0], customList[i]
+        heapify(customList, i , 0)
+
+    # customList.reverse()
+
+
+my_list = [3,5,0,6,2,1,4]
+heapSort(my_list)
 print(my_list)
